fcf1cb3903bcceea1514d201f0c6d906d215561c
Jezhora <jezhora.photo@gmail.com>
2024-02-09 23:08:25 +0300

    feat: 11 passed

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -211,9 +211,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let palidrom = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    palidrom += str[i];
+  }
+  return palidrom === str;
 }
+// console.log(isPalindrome('qweqwe'));
 
 /**
  * Finds the first occurrence of a letter in a string.
@@ -229,8 +234,13 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -248,8 +258,14 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const result = `${num}`;
+  for (let i = 0; i < result; i += 1) {
+    if (+result[i] === digit) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
@@ -265,8 +281,31 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let mid = 1;
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (i < mid) {
+      left += arr[i];
+    }
+    if (i > mid) {
+      right += arr[i];
+    }
+
+    if (i === arr.length - 1) {
+      if (left === right) {
+        return mid;
+      }
+      if (mid < arr.length - 2) {
+        mid += 1;
+        left = 0;
+        right = 0;
+        i = -1;
+      }
+    }
+  }
+  return -1;
 }
 
 /**
